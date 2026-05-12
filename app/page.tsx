a48d57acf37d5b3e022153f4d9414abc1c8d4c24
Naaz Ahmedi <naaz.ahmedi10@gmail.com>
2026-05-13 00:18:04 +0530

    New Version

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ import GiftBanner from "@/components/GiftBanner";
 import Categories from "@/components/Categories";
 import OrderMarquee from "@/components/OrderMarquee";
 import VisitUs from "@/components/VisitUs";
-import SwirlSection from "@/components/SwirlSection";
-import Toppings from "@/components/Toppings";
 import Footer from "@/components/Footer";
 
 export default function Home() {
@@ -13,11 +11,9 @@ export default function Home() {
     <main className="min-h-screen bg-[#FEF2F2]">
       <Hero />
       <div className="py-8 sm:py-12"><ProductCarousel /></div>
-      <div className="py-8 sm:py-12"><SwirlSection /></div>
       <div className="py-8 sm:py-12"><GiftBanner /></div>
       <div className="py-8 sm:py-12"><Categories /></div>
       <OrderMarquee />
-      <Toppings />
       <div className="py-8 sm:py-12"><VisitUs /></div>
       <Footer />
     </main>
